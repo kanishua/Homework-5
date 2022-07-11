ed5df7b408455e18d37c5db0c3cf2f7ea6adf37b
Olya Osypchuk <kanishua.o@gmail.com>
2022-07-11 22:26:30 +0300

    addddd

---
+++ Homework_Library_Tests/CyclesHelperTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_Library_Tests
+{
+    class CyclesHelperTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
