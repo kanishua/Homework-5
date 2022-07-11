cb058a1997d6110948bf1d8da38a3082902957e1
Olya Osypchuk <kanishua.o@gmail.com>
2022-07-11 12:54:24 +0300

    Add project files.

---
+++ Homework_Lection_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Lection_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
