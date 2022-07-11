01586eb5541a08b7717756bbd2bf33bb2eb49388
Olya Osypchuk <kanishua.o@gmail.com>
2022-07-11 18:18:03 +0300

    Add 3 class of library

--- Homework_Lection_5/Program.cs
+++ Homework_Lection_5/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homework_Lection_5_Library;
+using System;
 
 namespace Homework_Lection_5
 {
@@ -6,7 +7,70 @@ namespace Homework_Lection_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = ArrayHelper.GanerateSingleArray(10);
+            PrintArray(array);
+            Console.WriteLine();
+
+            Console.WriteLine("1.Find the minimum element of an array:");
+            Console.WriteLine(ArrayHelper.FindMinElement(array));
+            Console.WriteLine("2.Find the maximum element of an array:");
+            Console.WriteLine(ArrayHelper.FindMaxElement(array));
+            Console.WriteLine("3.Find the index of the minimum element of the array:");
+            Console.WriteLine(ArrayHelper.FindIndexMinElement(array));
+            Console.WriteLine("4.Find the index of the maximum element of the array:");
+            Console.WriteLine(ArrayHelper.FindIndexMaxElement(array));
+            Console.WriteLine("5.Calculate the sum of array elements with odd indices:");
+            Console.WriteLine(ArrayHelper.FindSumElementsWithOddIndex(array));
+            Console.WriteLine("6.Make an array reverse (an array in the opposite direction):");
+            ArrayHelper.MakeReverseArray(array);
+            PrintArray(array);
+            Console.WriteLine("7.Count the number of odd array elements:");
+            Console.WriteLine(ArrayHelper.CountNumberOddElements(array));
+            Console.WriteLine("8.Swap the first and second half of the array:");
+            ArrayHelper.SwapPlaceHalfArray(array);
+            PrintArray(array);
+            Console.WriteLine("9.Sort the array in ascending order in bubble way:");
+            ArrayHelper.SortArrayByBubble(array);
+            PrintArray(array);
+            Console.WriteLine("10.Sort the array in descending order in inserts way");
+            ArrayHelper.SortArrayByInsert(array);
+            PrintArray(array);
+            Console.WriteLine();
+
+            int[,] matrix = MatrixHelper.GanerateMatrix(5, 3);
+            PrintMatrix(matrix);
+            Console.WriteLine();
+
+            Console.WriteLine("1.Find the minimum element of an array:");
+            Console.WriteLine(MatrixHelper.FindMinElement(matrix));
+            Console.WriteLine("2.Find the maximum element of an array:");
+            Console.WriteLine(MatrixHelper.FindMaxElement(matrix));
+            Console.WriteLine("3.Find the index of the minimum element of the array:");
+            PrintArray(MatrixHelper.FindIndexMinElement(matrix));
+            Console.WriteLine("4.Find the index of the maximum element of the array:");
+            PrintArray(MatrixHelper.FindIndexMaxElement(matrix));
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                 Console.WriteLine();
+            }           
         }
     }
 }
---
+++ Homework_Lection_5_Library/MatrixHelper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_Lection_5_Library
+{
+    public class MatrixHelper
+    {
+        public static int[,] GanerateMatrix(int size1, int size2)
+        {
+            Random random = new Random();
+            int[,] matrix = new int[size1, size2];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(0, 10);
+                }
+            }
+
+            return matrix;
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //1.Find the minimum element of an array.
+        public static int FindMinElement(int[,] matrix)
+        {
+            int[] arrayOfIndexs = MatrixHelper.FindIndexMinElement(matrix);
+            return matrix[arrayOfIndexs[0], arrayOfIndexs[1]];
+        }
+
+        //2.Find the maximum element of an array.
+        public static int FindMaxElement(int[,] matrix)
+        {
+            int[] arrayOfIndexs = MatrixHelper.FindIndexMaxElement(matrix);
+            return matrix[arrayOfIndexs[0], arrayOfIndexs[1]];
+        }
+
+        //3.Find the index of the minimum element of the array.
+        public static int[] FindIndexMinElement(int[,] matrix)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                throw new ArgumentException("Empty or null matrix");
+            }
+
+            int indexIMinElement = 0;
+            int indexJMinElement = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < matrix[indexIMinElement, indexJMinElement])
+                    {
+                        indexIMinElement = i;
+                        indexJMinElement = j;
+                    }
+                }
+            }
+
+            return new int[] {indexIMinElement, indexJMinElement};
+        }
+
+        //4.Find the index of the maximum element of the array.
+        public static int[] FindIndexMaxElement(int[,] matrix)
+        {
+            if (matrix == null || matrix.Length == 0)
+            {
+                throw new ArgumentException("Empty or null matrix");
+            }
+
+            int indexIMaxElement = 0;
+            int indexJMaxElement = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > matrix[indexIMaxElement, indexJMaxElement])
+                    {
+                        indexIMaxElement = i;
+                        indexJMaxElement = j;
+                    }
+                }
+            }
+
+            return new int[] {indexIMaxElement, indexJMaxElement};
+        }
+
+        //5.Find the number of array elements that are greater than all their neighbors at the same time.
+
+        //6.Flip an array about its main diagonal.
+    }
+}
